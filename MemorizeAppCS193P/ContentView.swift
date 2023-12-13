341d4dcf24697f345e9ff975c590183d386eba06
ulasozturk <ulasozturkdev@gmail.com>
2023-12-13 14:00:37 +0300

    Initial Commit

---
+++ MemorizeAppCS193P/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemorizeAppCS193P
+//
+//  Created by macbook pro on 13.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemorizeAppCS193P/MemorizeAppCS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppCS193PApp.swift
+//  MemorizeAppCS193P
+//
+//  Created by macbook pro on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppCS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
